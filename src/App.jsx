d032df98005da9f3402ecb919b4c2217b9299cb4
balafoutisp <thrylos012@gmail.com>
2023-06-08 22:59:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickCarousel";
+
+const App = () => {
+  return (
+    <main>
+      {/*<Carousel />*/}
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
